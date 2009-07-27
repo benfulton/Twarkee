bc27f20bf7333c85d5e39eefad0ce1b204bc477d
Ben Fulton <benmarkfulton@gmail.com>
2009-07-27 19:31:59 -0400

    Add Options form

---
+++ Twarkee/OptionsForm.Designer.cs
@@ -0,0 +1,322 @@
+namespace Twarkee
+{
+    partial class OptionsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.radXmlButton = new System.Windows.Forms.RadioButton();
+            this.radJsonButton = new System.Windows.Forms.RadioButton();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.chkStartAtLogin = new System.Windows.Forms.CheckBox();
+            this.chkMinimizeToTray = new System.Windows.Forms.CheckBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.chkHandlePublicTimeline = new System.Windows.Forms.CheckBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtRefreshDelay = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnApply = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Location = new System.Drawing.Point(2, 8);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(486, 539);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.groupBox2);
+            this.tabPage1.Controls.Add(this.groupBox3);
+            this.tabPage1.Controls.Add(this.groupBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(478, 513);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "General";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.radXmlButton);
+            this.groupBox2.Controls.Add(this.radJsonButton);
+            this.groupBox2.Location = new System.Drawing.Point(2, 209);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(475, 95);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Select API";
+            // 
+            // radXmlButton
+            // 
+            this.radXmlButton.AutoSize = true;
+            this.radXmlButton.Checked = true;
+            this.radXmlButton.Location = new System.Drawing.Point(132, 42);
+            this.radXmlButton.Name = "radXmlButton";
+            this.radXmlButton.Size = new System.Drawing.Size(47, 17);
+            this.radXmlButton.TabIndex = 1;
+            this.radXmlButton.TabStop = true;
+            this.radXmlButton.Text = "XML";
+            this.radXmlButton.UseVisualStyleBackColor = true;
+            // 
+            // radJsonButton
+            // 
+            this.radJsonButton.AutoSize = true;
+            this.radJsonButton.Location = new System.Drawing.Point(16, 42);
+            this.radJsonButton.Name = "radJsonButton";
+            this.radJsonButton.Size = new System.Drawing.Size(53, 17);
+            this.radJsonButton.TabIndex = 0;
+            this.radJsonButton.Text = "JSON";
+            this.radJsonButton.UseVisualStyleBackColor = true;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.chkStartAtLogin);
+            this.groupBox3.Controls.Add(this.chkMinimizeToTray);
+            this.groupBox3.Location = new System.Drawing.Point(0, 310);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(468, 100);
+            this.groupBox3.TabIndex = 6;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Display Options";
+            // 
+            // chkStartAtLogin
+            // 
+            this.chkStartAtLogin.AutoSize = true;
+            this.chkStartAtLogin.Location = new System.Drawing.Point(23, 64);
+            this.chkStartAtLogin.Name = "chkStartAtLogin";
+            this.chkStartAtLogin.Size = new System.Drawing.Size(220, 17);
+            this.chkStartAtLogin.TabIndex = 5;
+            this.chkStartAtLogin.Text = "Start Twarkee automatically when I login.";
+            this.chkStartAtLogin.UseVisualStyleBackColor = true;
+            // 
+            // chkMinimizeToTray
+            // 
+            this.chkMinimizeToTray.AutoSize = true;
+            this.chkMinimizeToTray.Location = new System.Drawing.Point(23, 32);
+            this.chkMinimizeToTray.Name = "chkMinimizeToTray";
+            this.chkMinimizeToTray.Size = new System.Drawing.Size(139, 17);
+            this.chkMinimizeToTray.TabIndex = 4;
+            this.chkMinimizeToTray.Text = "Minimize to system tray?";
+            this.chkMinimizeToTray.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.groupBox1.Controls.Add(this.label11);
+            this.groupBox1.Controls.Add(this.chkHandlePublicTimeline);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.txtRefreshDelay);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtPassword);
+            this.groupBox1.Controls.Add(this.txtUserName);
+            this.groupBox1.Location = new System.Drawing.Point(3, 6);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(468, 200);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Account Details";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(16, 153);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(219, 13);
+            this.label11.TabIndex = 12;
+            this.label11.Text = "Would you like to include the public timeline?";
+            // 
+            // chkHandlePublicTimeline
+            // 
+            this.chkHandlePublicTimeline.AutoSize = true;
+            this.chkHandlePublicTimeline.Location = new System.Drawing.Point(282, 153);
+            this.chkHandlePublicTimeline.Name = "chkHandlePublicTimeline";
+            this.chkHandlePublicTimeline.Size = new System.Drawing.Size(15, 14);
+            this.chkHandlePublicTimeline.TabIndex = 11;
+            this.chkHandlePublicTimeline.UseVisualStyleBackColor = true;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(6, 117);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(126, 13);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "Refresh messages every:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(201, 117);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Minutes";
+            // 
+            // txtRefreshDelay
+            // 
+            this.txtRefreshDelay.Location = new System.Drawing.Point(138, 117);
+            this.txtRefreshDelay.Name = "txtRefreshDelay";
+            this.txtRefreshDelay.Size = new System.Drawing.Size(57, 20);
+            this.txtRefreshDelay.TabIndex = 2;
+            this.txtRefreshDelay.Text = "5";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Password:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "User Name:";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(9, 80);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(162, 20);
+            this.txtPassword.TabIndex = 1;
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Location = new System.Drawing.Point(9, 41);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(162, 20);
+            this.txtUserName.TabIndex = 0;
+            // 
+            // btnOk
+            // 
+            this.btnOk.Location = new System.Drawing.Point(239, 557);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(75, 23);
+            this.btnOk.TabIndex = 1;
+            this.btnOk.Text = "&OK";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(320, 557);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 2;
+            this.btnCancel.Text = "&Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnApply
+            // 
+            this.btnApply.Enabled = false;
+            this.btnApply.Location = new System.Drawing.Point(401, 557);
+            this.btnApply.Name = "btnApply";
+            this.btnApply.Size = new System.Drawing.Size(75, 23);
+            this.btnApply.TabIndex = 3;
+            this.btnApply.Text = "&Apply";
+            this.btnApply.UseVisualStyleBackColor = true;
+            this.btnApply.Click += new System.EventHandler(this.btnApply_Click);
+            // 
+            // OptionsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(490, 590);
+            this.Controls.Add(this.btnApply);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.tabControl1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "OptionsForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Twarkee Options";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnApply;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtRefreshDelay;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.CheckBox chkMinimizeToTray;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.CheckBox chkStartAtLogin;
+        private System.Windows.Forms.CheckBox chkHandlePublicTimeline;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton radXmlButton;
+        private System.Windows.Forms.RadioButton radJsonButton;
+    }
+}
---
+++ Twarkee/OptionsForm.cs
@@ -0,0 +1,110 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using SpeechLib;
+using Microsoft.Win32;
+
+namespace Twarkee
+{
+    public partial class OptionsForm : Form
+    {
+        public OptionsForm()
+        {
+            InitializeComponent();
+            this.Load += new EventHandler(OptionsForm_Load);
+        }
+
+        void OptionsForm_Load(object sender, EventArgs e)
+        {
+            LoadOptions();
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            if (txtUserName.Text.Trim() == string.Empty || txtPassword.Text.Trim() == string.Empty)
+            {
+                MessageBox.Show("User name and password are required.", "Account Details required", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+                return;
+            }
+            int delay = 0;
+            if (txtRefreshDelay.Text.Trim() == string.Empty || !int.TryParse(txtRefreshDelay.Text.Trim(), out delay))
+            {
+                MessageBox.Show("Refresh Delay is required and must be a valid number.", "Refresh Delay required", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+                return;
+            }
+            SaveOptions();
+            this.Close();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnApply_Click(object sender, EventArgs e)
+        {
+            SaveOptions();
+        }
+
+        private void LoadOptions()
+        {
+            txtUserName.Text = Properties.Settings.Default.UserName;
+            txtPassword.Text = Properties.Settings.Default.Password;
+            txtRefreshDelay.Text = Properties.Settings.Default.RefreshDelay.ToString();
+
+            chkMinimizeToTray.Checked = Properties.Settings.Default.MinimizeToTray;
+            chkStartAtLogin.Checked = (((string)Microsoft.Win32.Registry.GetValue("HKEY_CURRENT_USER\\Software\\Microsoft\\Windows\\CurrentVersion\\Run", "TeleTwitter", "")).Length > 0);
+            radJsonButton.Checked = (Properties.Settings.Default.ApiType == (int)TeleTwitter.Lib.ApiType.JSON);
+            radXmlButton.Checked = (Properties.Settings.Default.ApiType == (int)TeleTwitter.Lib.ApiType.XML);
+
+            chkHandlePublicTimeline.Checked = Properties.Settings.Default.UsePublicTimeline;
+
+        }
+
+        private void SaveOptions()
+        {
+            Properties.Settings.Default.UserName = txtUserName.Text;
+            Properties.Settings.Default.Password = txtPassword.Text;
+            Properties.Settings.Default.RefreshDelay = int.Parse(txtRefreshDelay.Text.Trim());
+            Properties.Settings.Default.MinimizeToTray = chkMinimizeToTray.Checked;
+
+            if (Properties.Settings.Default.UsePublicTimeline != chkHandlePublicTimeline.Checked)
+            {
+                MessageBox.Show("Please close and reopen TeleTwitter for your changes to take place.", "Restart TeleTwitter", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+            Properties.Settings.Default.UsePublicTimeline = chkHandlePublicTimeline.Checked;
+
+            if (chkStartAtLogin.Checked != (((string)Microsoft.Win32.Registry.GetValue("HKEY_CURRENT_USER\\Software\\Microsoft\\Windows\\CurrentVersion\\Run", "TeleTwitter", "")).Length > 0))
+            {
+                if (chkStartAtLogin.Checked)
+                {
+                    Microsoft.Win32.Registry.SetValue("HKEY_CURRENT_USER\\Software\\Microsoft\\Windows\\CurrentVersion\\Run", "TeleTwitter", System.Reflection.Assembly.GetEntryAssembly().Location);
+                }
+                else
+                {
+                    Microsoft.Win32.Registry.SetValue("HKEY_CURRENT_USER\\Software\\Microsoft\\Windows\\CurrentVersion\\Run", "TeleTwitter", "");
+                }
+            }
+
+            Properties.Settings.Default.ApiType = radJsonButton.Checked ? (int)TeleTwitter.Lib.ApiType.JSON : (int)TeleTwitter.Lib.ApiType.XML;
+			try
+			{
+				TeleTwitter.Lib.TwitterManager.Instance().TwitterApi = (TeleTwitter.Lib.ApiType)Properties.Settings.Default.ApiType;
+			}
+			catch
+			{
+				// just ignore login errors until after we've saved settings
+			}
+
+            Properties.Settings.Default.Save();
+        }
+
+
+
+    }
+}
